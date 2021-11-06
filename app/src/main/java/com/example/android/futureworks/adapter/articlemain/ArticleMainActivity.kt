fbdcd9e2139c8f692cddff35921e340a975bd11f
kevin.claassen <claassenk@outlook.com>
2021-11-06 20:32:40 +0100

    Confirmed working API response from to get Articles

--- app/src/main/java/com/example/android/futureworks/adapter/articlemain/ArticleMainActivity.kt
+++ app/src/main/java/com/example/android/futureworks/adapter/articlemain/ArticleMainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.android.futureworks.adapter.articlemain
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.futureworks.R
+import com.example.android.futureworks.network.ArticleApiService
+import com.example.android.futureworks.utils.Constants
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class ArticleMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_main)
+
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val service = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(ArticleApiService::class.java)
+
+        CoroutineScope(IO).launch {
+            val response = service.getArticles(
+                bearerToken = Constants.ACCESS_TOKEN
+            )
+            Log.d("Retrofit Response", "onCreate: ${response}")
+        }
     }
 }
--- app/src/main/java/com/example/android/futureworks/utils/Constants.kt
+++ app/src/main/java/com/example/android/futureworks/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.android.futureworks.utils
 
 object Constants {
     const val BASE_URL = "https://mobilecodetest.fws.dev/api/v1/"
-    const val ACCESS_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzU4NjcyMTYsIm5iZiI6MTYzNTg2NzIxNiwianRpIjoiYTg0OGRiMDYtYWJlZS00NTk5LWIwOWUtMmI4YjQzYmEzYmE1IiwiZXhwIjoxNjM1ODY4MTE2LCJpZGVudGl0eSI6ImNvZGUiLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.9bnIrh_FW7FHyrhAWgNlKJHEv3LjCZ-iWKJLQIpv-nU"
+    const val ACCESS_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzYyMjY5ODUsIm5iZiI6MTYzNjIyNjk4NSwianRpIjoiZTA1YTA5OTQtMGUwMS00MDg4LThiZjctMDhhNWVjNGE5YWIyIiwiZXhwIjoxNjM2MjI3ODg1LCJpZGVudGl0eSI6ImNvZGUiLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.PEE28wztAkDq0uulKJ7ONMD_rxdH0BJHCakeWwq5gNU"
 }
